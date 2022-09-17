f7e3f324cf4663a4b88a6c316f48598510bf5f32
SeokHwan An <asok369258@naver.com>
2022-09-17 22:12:37 +0900

    feature #2 : BaseEntity 생성

---
+++ festival/src/main/java/likelion/festival/entitiy/BaseEntity.java
@@ -0,0 +1,24 @@
+package likelion.festival.entitiy;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+}
