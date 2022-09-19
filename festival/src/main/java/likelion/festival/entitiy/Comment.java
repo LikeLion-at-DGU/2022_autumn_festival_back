347d93459ca29ba68819dd696259577c7d67f8d2
MINJU <alswn9938@gmail.com>
2022-09-19 13:11:02 +0900

    feature #13 : create Comment Entity

---
+++ festival/src/main/java/likelion/festival/entitiy/Comment.java
@@ -0,0 +1,32 @@
+package likelion.festival.entitiy;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comment")
+@Builder
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String password;
+
+    private String content;
+
+    // Booth
+//    @ManyToOne(targetEntity = Booth.class, fetch = FetchType.LAZY)
+//    @JoinColumn(name = "booth_id")
+//    private Booth booth;
+
+}
