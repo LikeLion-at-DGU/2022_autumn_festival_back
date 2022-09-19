bd8066b9a43ca03be1ae1ee062e9c1e6c715b8cd
SeokHwan An <asok369258@naver.com>
2022-09-19 23:06:40 +0900

    feature#16 : 에러 코드 커스텀 enum 생성

---
+++ festival/src/main/java/likelion/festival/exception/ExceptionCode.java
@@ -0,0 +1,17 @@
+package likelion.festival.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ExceptionCode {
+    WRONG_BOOTH_ID(HttpStatus.BAD_REQUEST.value(), "잘못된 페이지입니다.");
+
+    private final int code;
+    private final String message;
+
+    ExceptionCode(int code, String message){
+        this.code = code;
+        this.message = message;
+    }
+}
