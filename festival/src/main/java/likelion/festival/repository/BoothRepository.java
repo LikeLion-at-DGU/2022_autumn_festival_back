daf5817360112b2371bd5b8520fab1d58b084b8b
SeokHwan An <asok369258@naver.com>
2022-09-20 11:38:36 +0900

    feature #7 : BoothRepository 메소드 추가

--- festival/src/main/java/likelion/festival/repository/BoothRepository.java
+++ festival/src/main/java/likelion/festival/repository/BoothRepository.java
@@ -2,7 +2,23 @@ package likelion.festival.repository;
 
 
 import likelion.festival.entitiy.Booth;
+import likelion.festival.entitiy.BoothLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BoothRepository extends JpaRepository<Booth, Long> {
+
+    List<Booth> findByBoothLocation(BoothLocation boothLocation);
+
+    // TODO : 좋아요 기준으로 탑 3개 추출하는 것으로 refactoring 하기
+    @Query("SELECT e FROM Booth e ORDER BY e.title")
+    List<Booth> findByTop3();
+
+    @Query("SELECT e FROM Booth e where e.title = :title")
+    List<Booth> findByTitle(@Param("title") String title);
+
+    // TODO : 메뉴검색 문 추가
 }
