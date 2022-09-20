83484aa6e66ea78fc48e8e519f4ae73ba78421c8
SeokHwan An <asok369258@naver.com>
2022-09-20 09:33:01 +0900

    feature #19 : custom exception 처리 controller 추가

---
+++ festival/src/main/java/likelion/festival/exception/handler/advice/ExControllerAdvice.java
@@ -0,0 +1,18 @@
+package likelion.festival.exception.handler.advice;
+import likelion.festival.exception.ExceptionCode;
+import likelion.festival.exception.WrongBoothId;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongBoothId.class)
+    public ErrorResult wrongBoothId(WrongBoothId e) {
+        return new ErrorResult(ExceptionCode.WRONG_BOOTH_ID);
+    }
+
+}
