e2c00466a8c2e4d4702d172adbf6e2456266c747
이건회 <rg970604@naver.com>
2022-09-23 02:04:39 +0900

    feature #25 : BoothStringToRequestDtoConverter String Json값 BoothDto로 변환

---
+++ festival/src/main/java/likelion/festival/converter/BoothStringToRequestDtoConverter.java
@@ -0,0 +1,31 @@
+package likelion.festival.converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import likelion.festival.dto.BoothDto;
+import likelion.festival.dto.NotificationDto;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BoothStringToRequestDtoConverter extends Throwable implements Converter<String, BoothDto> {
+    private ObjectMapper objectMapper;
+
+    public BoothStringToRequestDtoConverter(ObjectMapper objectMapper){
+        this.objectMapper = objectMapper;
+    }
+
+    BoothDto boothDto;
+
+    @Override
+    public BoothDto convert(String source){
+        try{
+            boothDto = objectMapper.readValue(source, new TypeReference<BoothDto>() {
+            });
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+        return boothDto;
+    }
+}
