d7b4c1c08d3be84474ed29a8d728a21778fa19e3
이건회 <rg970604@naver.com>
2022-09-23 02:58:38 +0900

    feature #25 : Image Entity 수정

--- festival/src/main/java/likelion/festival/entitiy/Image.java
+++ festival/src/main/java/likelion/festival/entitiy/Image.java
@@ -1,11 +1,14 @@
 package likelion.festival.entitiy;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
 @Entity
+@NoArgsConstructor
 public class Image {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,11 +17,15 @@ public class Image {
 
     private String origin_file_name;
 
+    private String server_file_name;
+
     private String stored_file_path;
 
-    private long file_size;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "booth_id")
-    private Booth booth;
+    @Builder
+    public Image(Long id, String origin_file_name, String server_file_name, String stored_file_path) {
+        this.id = id;
+        this.origin_file_name = origin_file_name;
+        this.server_file_name = server_file_name;
+        this.stored_file_path = stored_file_path;
+    }
 }
