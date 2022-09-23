800fecf44fd95081bebe60fa025ec8848d39541f
이건회 <rg970604@naver.com>
2022-09-23 15:33:26 +0900

    feature #25 : ImageService @Transactional 어노테이션 변경

--- festival/src/main/java/likelion/festival/service/ImageService.java
+++ festival/src/main/java/likelion/festival/service/ImageService.java
@@ -10,8 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
-@Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Service
 public class ImageService {
 
     private final ImageRepository imageRepository;
@@ -21,7 +22,7 @@ public class ImageService {
         return imageRepository.save(imageDto.toEntity()).getId();
     }
 
-    @Transactional
+
     public ImageDto getImage(Long id) {
         Optional<Image> imageOptional = imageRepository.findById(id);
         if (!imageOptional.isPresent()) {
@@ -31,9 +32,9 @@ public class ImageService {
 
         ImageDto imageDto = ImageDto.builder()
                 .id(id)
-                .origin_file_name(image.getOrigin_file_name())
-                .server_file_name(image.getServer_file_name())
-                .stored_file_path(image.getStored_file_path())
+                .originFileName(image.getOriginFileName())
+                .serverFileName(image.getServerFileName())
+                .storedFilePath(image.getStoredFilePath())
                 .build();
         return imageDto;
     }
