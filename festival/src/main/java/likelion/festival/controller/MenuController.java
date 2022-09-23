5f33fd826bfcdf7f976ccf52c8553c64223a77b0
tkdwns414 <sangjune2000@naver.com>
2022-09-23 16:51:52 +0900

    feature #34: create MenuController

---
+++ festival/src/main/java/likelion/festival/controller/MenuController.java
@@ -0,0 +1,24 @@
+package likelion.festival.controller;
+
+import likelion.festival.dto.*;
+import likelion.festival.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("api/menus")
+@RestController
+public class MenuController {
+
+    private final MenuService menuService;
+
+    @PutMapping("/{id}")
+    public MenuResponseDto updateMenu(@PathVariable Long id, @RequestBody MenuRequestDto menuRequestDto){
+        return menuService.update(id, menuRequestDto);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteMenu(@PathVariable Long id){
+        menuService.delete(id);
+    }
+}
