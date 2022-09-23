c520d08df893240c6cb2b1ee2a6e26cd6c056dfa
SeokHwan An <asok369258@naver.com>
2022-09-23 10:19:58 +0900

    fix #28 : BoothRepository 수정

--- festival/src/main/java/likelion/festival/repository/BoothRepository.java
+++ festival/src/main/java/likelion/festival/repository/BoothRepository.java
@@ -2,23 +2,20 @@ package likelion.festival.repository;
 
 
 import likelion.festival.entitiy.Booth;
-import likelion.festival.entitiy.BoothLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BoothRepository extends JpaRepository<Booth, Long> {
 
-    List<Booth> findByBoothLocation(BoothLocation boothLocation);
+    List<Booth> findByLocation(String location);
 
     // TODO : 좋아요 기준으로 탑 3개 추출하는 것으로 refactoring 하기
     @Query("SELECT e FROM Booth e ORDER BY e.title")
     List<Booth> findByTop3();
 
-    @Query("SELECT e FROM Booth e where e.title = :title")
-    List<Booth> findByTitle(@Param("title") String title);
+    List<Booth> findByTitle(String title);
 
     // TODO : 메뉴검색 문 추가
 }
