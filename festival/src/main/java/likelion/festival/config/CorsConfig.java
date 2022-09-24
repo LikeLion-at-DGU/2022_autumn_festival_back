3d72d95c308e829c5fbb7a2529af2a0aa05f841b
SeokHwan An <asok369258@naver.com>
2022-09-24 21:08:03 +0900

    feature #27 : CorsConfig 수정

--- festival/src/main/java/likelion/festival/config/CorsConfig.java
+++ festival/src/main/java/likelion/festival/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package likelion.config;
+package likelion.festival.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://local:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
     }
 
