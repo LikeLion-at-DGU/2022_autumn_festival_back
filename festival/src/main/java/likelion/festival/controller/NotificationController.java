bbf26f7dbb8e8023be93472bebb195f43c6c5873
이건회 <rg970604@naver.com>
2022-09-27 20:24:12 +0900

    feature #25 : refactor Notification Controller

--- festival/src/main/java/likelion/festival/controller/NotificationController.java
+++ festival/src/main/java/likelion/festival/controller/NotificationController.java
@@ -8,28 +8,32 @@ import likelion.festival.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(value = "${app-notification}")
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
     private final ImageService imageService;
 
+    @ResponseBody
     @GetMapping("{id}")
     public NotificationDto readNotification(@PathVariable Long id){
         return notificationService.readNotification(id);
     }
 
+    @ResponseBody
     @GetMapping
     public ResponseEntity readNotificationAll(@RequestParam(required = false) NotificationType notificationType){
         return ResponseEntity.ok(notificationService.readNotificationAll(notificationType));
     }
 
+    @ResponseBody
     @PostMapping
     public Integer createNotification(@RequestPart(value = "imgList",required = false) List<MultipartFile> imgList,
                                       @RequestParam(value = "notification", required = false) NotificationDto notificationDto){
@@ -41,14 +45,15 @@ public class NotificationController {
         return HttpStatus.OK.value();
     }
 
+    @ResponseBody
     @DeleteMapping("{id}")
     public String deleteNotification(@PathVariable Long id){
         notificationService.deleteNotification(id);
         return "Ok";
     }
 
+    @ResponseBody
     @PutMapping("{id}")
-
     public ResponseEntity<Notification> updateNotification(@RequestBody NotificationDto request, @PathVariable Long id){
         return ResponseEntity.ok(notificationService.updateNotification(id, request));
     }
