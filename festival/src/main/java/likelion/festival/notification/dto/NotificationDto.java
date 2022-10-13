1694df88afb3f74b93e13c73566489b37d393514
이건회 <rg970604@naver.com>
2022-10-14 01:35:10 +0900

    refactor : NotificationDto.java

--- festival/src/main/java/likelion/festival/notification/dto/NotificationDto.java
+++ festival/src/main/java/likelion/festival/notification/dto/NotificationDto.java
@@ -7,13 +7,14 @@ import likelion.festival.notification.entity.NotificationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
