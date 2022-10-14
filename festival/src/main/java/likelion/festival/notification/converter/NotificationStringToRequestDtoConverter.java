2d6a1ee8b6cefbb127259f6943bafd85522b4262
이건회 <rg970604@naver.com>
2022-10-15 05:20:29 +0900

    refactor : NotificationStringToRequestDtoConverter.java

--- festival/src/main/java/likelion/festival/notification/converter/NotificationStringToRequestDtoConverter.java
+++ festival/src/main/java/likelion/festival/notification/converter/NotificationStringToRequestDtoConverter.java
@@ -3,13 +3,13 @@ package likelion.festival.notification.converter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import likelion.festival.notification.dto.NotificationDto;
+import likelion.festival.notification.dto.NotificationRequestDto;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationStringToRequestDtoConverter extends Throwable implements
-    Converter<String, NotificationDto> {
+    Converter<String, NotificationRequestDto> {
 
   private ObjectMapper objectMapper;
 
@@ -17,16 +17,16 @@ public class NotificationStringToRequestDtoConverter extends Throwable implement
     this.objectMapper = objectMapper;
   }
 
-  NotificationDto notificationDto;
+  NotificationRequestDto notificationRequestDto;
 
   @Override
-  public NotificationDto convert(String source) {
+  public NotificationRequestDto convert(String source) {
     try {
-      notificationDto = objectMapper.readValue(source, new TypeReference<NotificationDto>() {
+      notificationRequestDto = objectMapper.readValue(source, new TypeReference<NotificationRequestDto>() {
       });
     } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
-    return notificationDto;
+    return notificationRequestDto;
   }
 }
