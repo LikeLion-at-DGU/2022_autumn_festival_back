bdd7762c12dcadf789ba2b544343a40a769b5d23
이건회 <rg970604@naver.com>
2022-10-15 05:08:45 +0900

    refactor : Notification.java

--- festival/src/main/java/likelion/festival/notification/entity/Notification.java
+++ festival/src/main/java/likelion/festival/notification/entity/Notification.java
@@ -1,19 +1,17 @@
 package likelion.festival.notification.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.sun.istack.NotNull;
-import java.time.LocalDateTime;
 import likelion.festival.global.baseEntity.BaseEntity;
 import likelion.festival.image.entity.Image;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -38,8 +36,7 @@ public class Notification extends BaseEntity {
   @JsonIgnore
   private List<Image> images = new ArrayList<>();
 
-  public void updateNotification(String title, String writer, String content,
-      NotificationType notificationType, List<Image> images) {
+  public void updateNotification(String title, String writer, String content,NotificationType notificationType, List<Image> images) {
     this.title = title;
     this.writer = writer;
     this.content = content;
